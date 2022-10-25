2d74e54b7ddf0c21feb57b83c58d5d764845d1f6
jakeross <jirhiker@gmail.com>
2022-10-24 20:18:07 -0600

    added more elements to st2 report

--- api/main.py
+++ api/main.py
@@ -4,9 +4,9 @@ import requests
 app = FastAPI()
 
 
+ST2 = 'https://st2.newmexicowaterdata.org/FROST-Server/v1.1'
 def st2_count(tag, f=None):
-    base = 'https://st2.newmexicowaterdata.org/FROST-Server/v1.1'
-    url = f'{base}/{tag}?$count=true&$top=1'
+    url = f'{ST2}/{tag}?$count=true&$top=1'
     if f:
         url = f'{url}&{f}'
 
@@ -17,6 +17,11 @@ def st2_count(tag, f=None):
         return 0
 
 
+def st2_get(tag):
+    resp = requests.get(f'{ST2}/{tag}')
+    return resp.json()
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -24,9 +29,17 @@ async def root():
 
 @app.get('/st2_report')
 async def get_st2_report():
+    obsprops = st2_get("ObservedProperties")
+    print(obsprops)
+    obsprops = [o['name'] for o in obsprops['value']]
+
     return {"locations": st2_count("Locations"),
             "things": st2_count("Things"),
-            "Depth To Water Datastreams": st2_count("Datastreams", f="$filter=name eq 'Groundwater Levels'")
+            "datastreams": st2_count("Datastreams"),
+            "observations": st2_count("Observations"),
+            "depth_to_water_datastreams": st2_count("Datastreams", f="$filter=name eq 'Groundwater Levels'"),
+            "datastream_names": 0,
+            "observed_properties": obsprops
             }
 
 
