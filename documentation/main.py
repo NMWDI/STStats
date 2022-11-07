9cd75998d996112e509971d9328ebcbf6b490a89
Jake Ross <jirhiker@gmail.com>
2022-11-07 11:03:36 -0700

    improved tutorial

--- documentation/main.py
+++ documentation/main.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+DESCRIPTIONS = ['Start', 'Getting More Data', 'Writing to CSV']
 
-@app.route("/tutorial/<page>")
+@app.route("/tutorial/<int:page>")
 def tutorial(page):
-    return render_template(f'tutorial{page}.html')
+    page_description = DESCRIPTIONS[page]
+
+    return render_template(f'tutorial{page}.html',
+                           page=page, page_description=page_description)
 
 
 @app.route("/")
