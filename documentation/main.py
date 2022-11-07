60b2712a074b3644a61317b1e27bd55e67d413e1
jakeross <jirhiker@gmail.com>
2022-11-07 08:17:38 -0700

    added pages to tutorial

--- documentation/main.py
+++ documentation/main.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/tutorial")
-def tutorial_simple():
-    return render_template('tutorial.html')
+@app.route("/tutorial0")
+def tutorial0():
+    return render_template('tutorial0.html')
+
+
+@app.route("/tutorial1")
+def tutorial1():
+    return render_template('tutorial1.html')
 
 
 @app.route("/")
