fdaecbd200a7f3c74276a5d2f3d1ac56b001e39e
jakeross <jirhiker@gmail.com>
2022-11-06 18:21:47 -0700

    added tutorial

--- documentation/main.py
+++ documentation/main.py
@@ -3,8 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/tutorial")
+def tutorial_simple():
+    return render_template('tutorial.html')
+
+
 @app.route("/")
 def help_link():
     return render_template('help_link.html')
+
 
 
