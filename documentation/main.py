18aae6fe04eaa43951a520a9d2f04d70740b8b63
jakeross <jirhiker@gmail.com>
2022-11-07 09:03:33 -0700

    added csv tutorial

--- documentation/main.py
+++ documentation/main.py
@@ -3,14 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/tutorial0")
-def tutorial0():
-    return render_template('tutorial0.html')
-
-
-@app.route("/tutorial1")
-def tutorial1():
-    return render_template('tutorial1.html')
+@app.route("/tutorial/<page>")
+def tutorial(page):
+    return render_template(f'tutorial{page}.html')
 
 
 @app.route("/")
