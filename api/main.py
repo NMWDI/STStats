8c7cf5f6f24bd5ca713bc5692c9f916b57e12e04
Jake Ross <jirhiker@gmail.com>
2022-11-16 14:14:12 -0700

    added scoreboard

--- api/main.py
+++ api/main.py
@@ -70,17 +70,17 @@ def aggregate_stats():
 
 def agency_gwl_observations(url, agency):
     obs = 0
-    print('agency observations')
-    for locations in agency_locations(url, agency):
+    print(f'agency observations ==== {agency}')
+    for i, locations in enumerate(agency_locations(url, agency)):
         lobs = 0
         # for i, l in enumerate(locations[:1]):
-        for i, l in enumerate(locations):
+        for j, l in enumerate(locations):
             for t in l['Things']:
                 for d in t['Datastreams']:
                     if d['name'] == 'Groundwater Levels':
                         lobs += st_count(url, f"Datastreams({d['@iot.id']})/Observations")
             obs += lobs
-            print(i, l['name'], lobs, obs)
+            print(i*1000+j, l['name'], lobs, obs)
         # break
 
     return obs
@@ -147,7 +147,7 @@ t.start()
 
 @app.get('/st2_report')
 async def get_st2_report():
-    return st_report(ST2, 'st2')
+    return cached_report
 
 
 LOCATION_SCHEMA = None
