ab1ffcee1097a6fc5405f39300ae3c462d3fcd1a
Jacob Brown <brown.jacob.alexander@gmail.com>
2024-01-23 11:43:18 -0700

    Added QGIS to top bar

--- main.py
+++ main.py
@@ -23,22 +23,22 @@ def qgis():
 
 @app.route("/browser_examples")
 def examples():
-    examples = [('Get all Locations from EBID',
-                 "https://st2.newmexicowaterdata.org/FROST-Server/v1.1/Locations?$filter=properties/agency eq 'EBID'"),
-                ('Get all Locations and Things from EBID',
-                 "https://st2.newmexicowaterdata.org/FROST-Server/v1.1/Locations?$filter=properties/agency eq "
-                 "'EBID'&$expand=Things"),
-                ("Get all Locations and Things with a Location name that startswith 'AR-'<br/>",
-                 "https://st2.newmexicowaterdata.org/FROST-Server/v1.1/Locations?$filter=startswith(name,"
-                 "'AR-')&$expand=Things"),
-                ("Get all Locations and Things within a given bounding box defined by latitudes and longitudes",
-                 "https://st2.newmexicowaterdata.org/FROST-Server/v1.1/Locations?$filter=st_within(location, "
-                 "geography'POLYGON((-106.9466 34.1201,-106.8038 34.1201,-106.8038 34.0086,-106.9466 34.0086,-106.9466 34.1201))')&$expand=Things"),
-                ("Get all datastreams with name 'Groundwater level(Pressure)'",
-                 "https://st2.newmexicowaterdata.org/FROST-Server/v1.1/Datastreams?$filter=name eq 'Groundwater level(Pressure)'"),
-                ]
+    st2_examples = [('Get all Locations from EBID',
+                    "https://st2.newmexicowaterdata.org/FROST-Server/v1.1/Locations?$filter=properties/agency eq 'EBID'"),
+                    ('Get all Locations and Things from EBID',
+                    "https://st2.newmexicowaterdata.org/FROST-Server/v1.1/Locations?$filter=properties/agency eq "
+                    "'EBID'&$expand=Things"),
+                    ("Get all Locations and Things with a Location name that startswith 'AR-'<br/>",
+                    "https://st2.newmexicowaterdata.org/FROST-Server/v1.1/Locations?$filter=startswith(name,"
+                    "'AR-')&$expand=Things"),
+                    ("Get all Locations and Things within a given bounding box defined by latitudes and longitudes",
+                    "https://st2.newmexicowaterdata.org/FROST-Server/v1.1/Locations?$filter=st_within(location, "
+                    "geography'POLYGON((-106.9466 34.1201,-106.8038 34.1201,-106.8038 34.0086,-106.9466 34.0086,-106.9466 34.1201))')&$expand=Things"),
+                    ("Get all datastreams with name 'Groundwater level(Pressure)'",
+                    "https://st2.newmexicowaterdata.org/FROST-Server/v1.1/Datastreams?$filter=name eq 'Groundwater level(Pressure)'"),
+                    ]
 
-    return render_template('browser_examples.html', examples=examples)
+    return render_template('browser_examples.html', st2_examples=st2_examples)
 
 @app.route("/apistore")
 def apistore():
